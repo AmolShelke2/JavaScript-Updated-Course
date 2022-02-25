175858de8372353f54541fa53baf29cf3ff6a1b8
Amol shelke <shelkeamol897@gmail.com>
2022-02-25 19:32:05 +0530

    Update If else and Challenge #2

--- section I/script.js
+++ section I/script.js
@@ -184,3 +184,58 @@ console.log(amolNew);
 console.log(`Just a simple string...`);
 
 */
+
+// Taking Decision if else statements
+
+/*
+const age = 13;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log("Sarah Can start driving license🥳");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :`);
+}
+
+const birthYear = 1992;
+let century;
+
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
+
+*/
+
+// Coding Challenge #2
+
+/*
+Use the BMI example from challenge #1, and the code you already 
+wrote, and improve it
+
+1. Print a nice output to the console, saying who has 
+the higher BMI, The message can be either "Mark's BMI is 
+higher than John's!" or "John's BMI is higher than Mark's!"
+
+2. Use a template string include the BMI values in the outputs.
+Example: "Mark's BMI (28.3) is higher than John's (23.9)"
+
+*/
+
+const marksMass = 78;
+const marksHeight = 1.69;
+const johnsMass = 92;
+const johnsHeight = 1.95;
+
+const marksBMI = marksMass / (marksHeight * marksHeight);
+const johnsBMI = johnsMass / (johnsHeight * johnsHeight);
+
+if (marksBMI > johnsBMI) {
+  console.log(`Mark's BMI ${marksBMI} is higher than John's ${johnsBMI}!`);
+} else {
+  console.log(`John's BMI ${johnsBMI} is higher than Mark's ${marksBMI}!`);
+}
