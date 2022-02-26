c47f2528013262502cea86ba9bfc369ea7c7850b
Amol shelke <shelkeamol897@gmail.com>
2022-02-26 13:09:51 +0530

    Update Truthy Falsy values

--- section I/script.js
+++ section I/script.js
@@ -243,6 +243,7 @@ if (marksBMI > johnsBMI) {
 // Type conversion and Coercion
 
 // type Conversion
+/*
 
 const inputYear = "1991";
 console.log(Number(inputYear), inputYear);
@@ -263,3 +264,30 @@ console.log("24" > "20"); // true
 let n = "1" + 1; // 11
 n = n - 1; // 11 - 1
 console.log(n); // 10
+
+*/
+
+// Truthy and Falsy values
+
+// 5 falsy values '', 0, undefined, null,  NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("amol"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 10;
+if (money) {
+  console.log("Don't spend it all :)");
+} else {
+  console.log("You should get a job!");
+}
+
+let height;
+
+if (height) {
+  console.log("Yes height is defined");
+} else {
+  console.log("height is UNDEFINED");
+}
