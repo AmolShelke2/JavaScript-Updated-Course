6a44fb59bbd5114de8dee45b70696ebe92d1e1e4
Amol shelke <shelkeamol897@gmail.com>
2022-02-27 17:29:37 +0530

    Updated Strict mode

--- Section II/script.js
+++ Section II/script.js
@@ -1,1 +1,16 @@
 // JavaScript Fundamentals Section II
+
+// Activating Strict mode
+/*
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 22;
+
+*/
