8bd1bd04a0b5dc759b9f911f6a231b310b41a0ef
Amol shelke <shelkeamol897@gmail.com>
2022-03-08 18:08:51 +0530

    Update final solution

--- Section II/script.js
+++ Section II/script.js
@@ -685,4 +685,30 @@ tips and totals arrays 😉
   
   4.3. Call the function with the 'totals' array
 GOOD LUCK 😀
+
+*/
+
+// Final Solution
+/*
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86,52]
+const tips = []
+const totals = []
+
+function calcTip(bill) {
+  if (bill >= 50 && bill < 300) {
+   return  bill * .15
+  } else
+    return bill * .20
+}
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = (calcTip(bills[i]))
+  tips.push(tip)
+  const total = bills[i] + calcTip(bills[i])
+  totals.push(total)
+}
+
+console.log(bills, tips, totals)
+
 */
