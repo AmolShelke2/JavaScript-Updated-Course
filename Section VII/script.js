73f769fcc75ca4ae178ddf0e425e34668fed8078
Amol shelke <shelkeamol897@gmail.com>
2022-03-12 18:18:53 +0530

    Update selecting and Manipulating Data

---
+++ Section VII/script.js
@@ -0,0 +1,9 @@
+"Use strict";
+
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number 🔥";
+
+document.querySelector(".number").textContent = 14;
+document.querySelector(".score").textContent = 20;
+document.querySelector(".guess").value = 20;
+console.log(document.querySelector(".guess").value);
