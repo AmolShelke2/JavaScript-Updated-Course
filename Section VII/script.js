0cdbb5d6530a7fda6b9aa0731a45c59ab5454042
Amol shelke <shelkeamol897@gmail.com>
2022-03-12 21:54:42 +0530

    working on coding Challenge #1

--- Section VII/script.js
+++ Section VII/script.js
@@ -52,3 +52,26 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   }
 });
+
+// Coding Challenge #1
+
+/*
+Implement a game reset functionality, so that player can make a new guess!
+Here it how!
+
+1. select the element with the 'again' class and attach a click event 
+handler
+
+2. In the handler function, restore initial values of the score and guess
+input field
+
+3. Restore the initial conditions of the message, number, score and guess
+input field
+
+4. Also restore the original background color (#222) and number width (15rem)
+
+*/
+
+document.querySelector(".again").addEventListener("click", function () {
+  document.querySelector(".score").textContent = 0;
+});
