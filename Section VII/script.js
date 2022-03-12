bcca45963cf7009bbc3fdf3f613d8efe683d7f5b
Amol shelke <shelkeamol897@gmail.com>
2022-03-12 19:04:30 +0530

    Update Implementing the Game logic

--- Section VII/script.js
+++ Section VII/script.js
@@ -1,9 +1,47 @@
 "Use strict";
 
+/*
 console.log(document.querySelector(".message").textContent);
-document.querySelector(".message").textContent = "Correct Number 🔥";
+
 
 document.querySelector(".number").textContent = 14;
 document.querySelector(".score").textContent = 20;
 document.querySelector(".guess").value = 20;
 console.log(document.querySelector(".guess").value);
+
+*/
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
+let score = 20;
+
+// Handeling click events
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector(".message").textContent = "No Number ⛔";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number";
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "💹 To High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "💹 To High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "💥 You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
