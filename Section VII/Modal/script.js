7409d0f49307e78f30fb725c53b1475e23c6741e
Amol shelke <shelkeamol897@gmail.com>
2022-03-15 17:19:52 +0530

    Update working with classes

--- Section VII/Modal/script.js
+++ Section VII/Modal/script.js
@@ -4,8 +4,20 @@ const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
 const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
-console.log(btnsOpenModal)
 
-for (let i = 0; i < btnsOpenModal.length; i++) 
-  console.log(btnsOpenModal[i].textContent)
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
+}
+
+const closeModal = function () {
+   modal.classList.add('hidden')
+   overlay.classList.add('hidden')
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+  
  
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click',closeModal)
