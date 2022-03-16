31018cbdae20785da337f40a6c4c95bb6a8cb4c7
Amol shelke <shelkeamol897@gmail.com>
2022-03-16 20:21:39 +0530

    Update An High Level Overview of JavaScript

--- section VIII/script.js
+++ section VIII/script.js
@@ -1,1 +1,17 @@
 'use strict'
+ 
+// An high-Level Overview of JavaScript
+
+/* 
+
+1. High Level
+2. Garbage collected
+3. Interpreted or just in time compiled
+4. Multi-paradigm
+5. Prototype-based object oriented
+6. first class functions
+7. Dynamic
+8. Single threded
+9. Non-Blocking event loop
+
+*/
