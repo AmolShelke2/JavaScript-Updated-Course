60f03b580ac8f717c501113fd7c4a923a02e05e6
Amol shelke <shelkeamol897@gmail.com>
2022-03-20 22:44:13 +0530

    UPDATE SPREAD OPERATOR

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -143,3 +143,104 @@ console.log(`Open: ${open} Close: ${close}`);
 */
 
 // The Spread Operator
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelor Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Carpse Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 11,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order recived ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+};
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(newArr); // otuput [1, 2, 7, 8, 9]
+console.log(...newArr); // output 1, 2, 7, 8, 9
+
+const newMenu = [...restaurant.mainMenu, "Gnocci"];
+console.log(newMenu);
+
+// copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+// Iterables are arrays, strings, maps, sets, NOT objects
+
+const str = "Amol";
+const letters = [...str, "", "S."];
+
+console.log(letters);
+console.log(...str);
+// console.log(`${...str}`) // it will show an error because
+
+// Real world example
+
+// const ingredients = [
+//   prompt("Lets make pasta! Ingredient 1"),
+//   prompt("Ingredients 2"),
+//   prompt("Ingredients 3"),
+// ];
+
+// console.log(ingredients);
+// restaurant.orderPasta(ingredients[0], ingredients[1], ingredients[2]); // Old way
+
+// restaurant.orderPasta(...ingredients); // With ES6 Spread Operator
+
+// Objects copy with spread operator
+const newRestaurant = {
+  ...restaurant,
+  founder: "Amol Shelke",
+  founded: 1998,
+  name: "Amols Restaurant",
+};
+
+console.log(newRestaurant);
+
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = "Amols restaurant";
+console.log("OLD ONE:", restaurant.name);
+console.log("NEW ONE:", restaurantCopy.name);
