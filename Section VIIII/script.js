11ab33812e1c729cccdf716f4038313cf95a59d4
Amol shelke <shelkeamol897@gmail.com>
2022-03-20 15:14:12 +0530

    Update Destructing Arrays

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -1,1 +1,57 @@
 // Destrcting arrays
+
+'use strict'
+
+const restaurant = {
+    name: 'Classico Italiano',
+    location: 'Via Angelor Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Carpse Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+    }
+}
+
+const arr = [2, 3, 4]
+const a = arr[0]
+const b = arr[1]
+const c = arr[2]
+console.log(a, b, c)
+
+// destructuring
+
+const [x, y, z] = arr
+console.log(x, y, z)
+
+let [main, , secondary] = restaurant.categories
+console.log(main, secondary)
+
+// Simple way
+ 
+// switching variables
+const temp = main
+main = secondary
+secondary = temp
+console.log(main, secondary)
+
+// [main, secondary] = [secondary, main]
+// console.log(main, secondary)
+
+
+// recive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0)
+console.log(starter, mainCourse)
+
+// nested desturucturing
+
+const nested = [2, 4, [5, 6]];
+// const [i, ,  j] = nested
+// console.log(i, j)
+
+const [i, ,  [j, k]] = nested
+console.log(i, j, k)
+
+// Default values
+const [p=1, q=1, r=1] = [8] 
+console.log(p, q, r)
