199d12274ad616b673f5e98a320ddcd9b08814b2
Amol shelke <shelkeamol897@gmail.com>
2022-03-20 16:16:44 +0530

    UPDATE DESTRUCTURING OBJECT

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 // Destructing arrays
 /*
@@ -58,4 +58,85 @@ console.log(p, q, r)
 
 */
 
-// Destructing Objects
+// Destructuring Objects
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelor Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Carpse Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 11,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order recived ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: "22:30",
+  address: "Via del sole, 21",
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: "Via del sole, 21",
+  starterIndex: 1,
+});
+
+const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories)
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+// console.log(restaurantName, hours, tags)
+
+// Default values
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters)
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+
+const {
+  fri: { open, close },
+} = openingHours;
+
+console.log(`Open: ${open} Close: ${close}`);
