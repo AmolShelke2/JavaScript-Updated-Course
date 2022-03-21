f90a07584654af8517c9d0f80882bd4f8015211e
Amol shelke <shelkeamol897@gmail.com>
2022-03-21 19:00:01 +0530

    Update the REST OPERATOR

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -248,3 +248,82 @@ console.log("NEW ONE:", restaurantCopy.name);
 */
 
 // Rest Patterns and Parameters
+/*
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelor Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Carpse Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 11,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order recived ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPizza: function (mainIngredients, ...otherIngredients) {
+    console.log(mainIngredients, otherIngredients);
+  },
+};
+
+// Spread, Because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+console.log(arr);
+
+// REST, because on left side 0f =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [Pizza, , risotto, ...otheFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(Pizza, risotto, otheFood);
+
+// objects
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(1, 2, 3, 4, 5, 6);
+
+const x = [22, 5, 7];
+add(...x);
+
+restaurant.orderPizza("mushrooms", "onions", "cheese", "olives", "Spimach");
+restaurant.orderPizza("mushrooms");
+*/
