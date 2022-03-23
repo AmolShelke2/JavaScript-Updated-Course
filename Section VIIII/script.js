4aeae984e63c2b90c6ef605c98793cbce8af1e76
Amol shelke <shelkeamol897@gmail.com>
2022-03-23 16:47:02 +0530

    UPDATE OPTIONAL CHAINING

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -329,7 +329,7 @@ restaurant.orderPizza("mushrooms");
 */
 
 // Short Circutting(&& and _)
-/*
+
 const restaurant = {
   name: "Classico Italiano",
   location: "Via Angelor Tavanti 23, Firenze, Italy",
@@ -371,7 +371,7 @@ const restaurant = {
     console.log(mainIngredients, otherIngredients);
   },
 };
-*/
+
 // Use any data type, return ANY data type, short-circuting
 
 /*
@@ -618,3 +618,31 @@ const restaurant = {
 */
 
 // Optional chaining
+if (restaurant.openingHours.mon) {
+  console.log(restaurant.openingHours.mon.open);
+}
+
+// without optional chaining
+// console.log(restaurant.openingHours.mon.open); // Cannot read properties of undefined
+
+// WITH OPTIONAL CHAINING
+console.log(restaurant.openingHours.mon?.open); // undefined
+console.log(restaurant.openingHours?.mon?.open);
+
+// Example;
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, We open at ${open}`);
+}
+
+// Methods
+console.log(restaurant.order?.(0, 1) ?? "Method does not exist");
+// console.log(restaurant.orderRosito?.(0, 1) ?? "Method does not exist");
+
+// Arrays
+
+const users = [{ name: "Amol", email: ".com" }];
+console.log(users[0]?.name ?? "user array empty");
