668edca6f5659d96b68d05c754013f45c53c1ba2
Amol shelke <shelkeamol897@gmail.com>
2022-03-23 17:22:18 +0530

    UPDATE LOOPING OBJECTS

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -651,3 +651,28 @@ console.log(users[0]?.name ?? "user array empty");
 */
 
 // Looping Objects Object keys, values and entries
+
+// properties name
+
+const properties = Object.keys(restaurant.openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for (const day of Object.keys(restaurant.openingHours)) {
+  openStr += `${day}, `;
+}
+
+console.log(openStr);
+
+// Property values
+const values = Object.values(restaurant.openingHours);
+// console.log(values);
+
+// entries
+const entries = Object.entries(restaurant.openingHours);
+// console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
