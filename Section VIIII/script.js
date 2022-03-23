08dbca5405a817cfcc167a4ea4b45350f58d25a7
Amol shelke <shelkeamol897@gmail.com>
2022-03-23 20:04:16 +0530

    UPDATE SOLUTION

--- Section VIIII/script.js
+++ Section VIIII/script.js
@@ -680,14 +680,14 @@ for (const [key, { open, close }] of entries) {
 
 */
 
-// CODING CHALLENGE #2
 ///////////////////////////////////////
 // Coding Challenge #2
 
 /* 
 Let's continue with our football betting app!
+
 1. Loop over the game.scored array and print each player name to the console, along with the goal
- number (Example: "Goal 1: Lewandowski")
+ number (Example: "Goal 1: Lewanddowski")
 
 2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate 
   averages, you can go check if you don't remember)
@@ -708,6 +708,69 @@ properties, and the number of goals as the value. In this game, it will look lik
         Hummels: 1,
         Lewandowski: 2
       }
-      
+
 GOOD LUCK 😀
 */
+
+// Solution
+
+const game = {
+  team1: "Bayern Munich",
+  team2: "Borrussia Dortmund",
+  players: [
+    [
+      "Neuer",
+      "Pavard",
+      "Martinez",
+      "Alaba",
+      "Davies",
+      "Kimmich",
+      "Goretzka",
+      "Coman",
+      "Muller",
+      "Gnarby",
+      "Lewandowski",
+    ],
+    [
+      "Burki",
+      "Schulz",
+      "Hummels",
+      "Akanji",
+      "Hakimi",
+      "Weigl",
+      "Witsel",
+      "Hazard",
+      "Brandt",
+      "Sancho",
+      "Gotze",
+    ],
+  ],
+  score: "4:0",
+  scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+  date: "Nov 9th, 2037",
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+// Task 1
+
+for (const [i, player] of game.scored.entries()) {
+  console.log(`Goal ${i + 1}: ${player}`);
+}
+
+// Task 2
+const odds = Object.values(game.odds);
+let average = 0;
+for (const odd of odds) average += odd;
+average /= odds.length;
+console.log(average);
+
+// Task 3
+for (const [team, draw] of Object.entries(game.odds)) {
+  // console.log(team, draw);
+  const message = team === "x" ? `draw` : `Victory ${game[team]};`;
+  console.log(`Odd of ${message} ${draw}`);
+}
