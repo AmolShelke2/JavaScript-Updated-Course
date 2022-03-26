f7c3908b46d7b8d5b6ceed2719dee70670a87321
Amol shelke <shelkeamol897@gmail.com>
2022-03-26 19:40:43 +0530

    UPDATE DEFAULT PARAMETER

--- Section X/script.js
+++ Section X/script.js
@@ -1,3 +1,24 @@
 'use strict';
 
 // Default parameter
+
+const bookings = [];
+
+const createBooking = function (flightNum, numPassenger = 1, price = 199) {
+  // Es5
+  //   numPassenger = numPassenger || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('Lauda', 2, 1000);
+createBooking('LH124', undefined, 1000);
