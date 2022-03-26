c78f191bc040232e5c62de9f874318bf4c9c16ff
Amol shelke <shelkeamol897@gmail.com>
2022-03-26 21:47:05 +0530

    UPDATE HOW PASSIGN ARGUMENT WORK VALUES VS REFRENCE

--- Section X/script.js
+++ Section X/script.js
@@ -24,4 +24,37 @@ createBooking('Lauda', 2, 1000);
 createBooking('LH124', undefined, 1000);
 
 */
+
 // How passing Arguments works values vs refrence.
+
+const flight = 'LH234';
+const Amol = {
+  name: 'Amol Shelke',
+  passPort: 696969969696,
+};
+
+const checkIn = function (flightNum, Passenger) {
+  flightNum = 'LH999';
+
+  Passenger.name = 'Mr. ' + Passenger.name;
+  if (Passenger.passPort === 696969969696) {
+    alert('Checked In');
+  } else {
+    alert('Wrong PassPort');
+  }
+};
+
+// checkIn(flight, Amol);
+// console.log(flight);
+// console.log(Amol);
+
+// Is the same as doing...
+// const flightNum = flight;
+// const Passenger = Amol;
+
+const newPassPort = function (person) {
+  person.passPort = Math.trunc(Math.random() * 1000000);
+};
+
+newPassPort(Amol);
+checkIn(flight, Amol);
