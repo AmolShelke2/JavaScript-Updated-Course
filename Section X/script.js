04e96829193cb601ea55d9fb5aef58e2e1e8adf9
Amol shelke <shelkeamol897@gmail.com>
2022-03-27 21:15:54 +0530

    UPDATE BIND METHOD

--- Section X/script.js
+++ Section X/script.js
@@ -239,3 +239,72 @@ book.call(india, ...flightData);
 */
 
 // The Bind method
+/*
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+const bookEW = book.bind(eurowings);
+const bookLM = book.bind(lufthansa);
+
+bookEW(23, 'Amol Shelke');
+
+const bookEw23 = book.bind(eurowings, 23);
+bookEw23('Amol Shelke');
+bookEw23('Amol Shelke2');
+
+// With Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+// lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial Application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVaI = addTax.bind(null, 0.18);
+// addVat = value => value + value * .18
+
+console.log(addVaI(200));
+// console.log(addVaI(28));
+
+// Challenge Rewrite the function with the technique function returning
+
+const addTax2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVaII = addTax2(0.18);
+console.log(addVaII(28));
+console.log(addVaII(200));
+
+*/
