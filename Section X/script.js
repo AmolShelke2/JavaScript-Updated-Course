186326c3ac3374482acccd7b3d49bfc18193e3e4
Amol shelke <shelkeamol897@gmail.com>
2022-03-29 20:37:36 +0530

    Update more example of closures

--- Section X/script.js
+++ Section X/script.js
@@ -450,3 +450,47 @@ console.dir(booker);
 */
 
 // More examples of closures
+
+// Example 1
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Reasigning f function
+h();
+f();
+
+console.dir(f);
+
+// Example 2
+
+const boardPaseenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passenger`);
+    console.log(`There are 3 groups, each with ${perGroup} passenger`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPaseenger(180, 3);
