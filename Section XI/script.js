9879224d45cc90f43d7e37fa72bf92916ae3332d
Amol shelke <shelkeamol897@gmail.com>
2022-03-30 21:35:37 +0530

    Update Creating Element

--- Section XI/script.js
+++ Section XI/script.js
@@ -61,6 +61,26 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+// Creating DOM Elements
+const displayMoveMents = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+        <div class="movements__row">
+          <div class="movements__type 
+          movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMoveMents(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -169,5 +189,3 @@ currenciesUnique.forEach(function (value, key, map) {
 */
 
 // Project Bankist_app
-
-// Creating DOM Elements
