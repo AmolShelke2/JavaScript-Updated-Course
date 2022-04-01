2c1ee4a6f0a5ab271a13d4d8c2d9a176be6678f6
Amol shelke <shelkeamol897@gmail.com>
2022-04-01 11:08:06 +0530

    Update the filter method and exercise solution:

--- Section XI/script.js
+++ Section XI/script.js
@@ -327,3 +327,32 @@ const movementsDescription = movements.map((mov, i) => {
 
 console.log(movementsDescription);
 */
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const deposits = movements.filter(function (mov) {
+  return mov > 0;
+});
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) {
+  if (mov > 0) {
+    depositsFor.push(mov);
+  }
+}
+
+console.log(depositsFor);
+
+// Exercise Create a array for withdrawals
+const withdrawls = movements.filter(function (mov) {
+  return mov < 0;
+});
+
+console.log(withdrawls);
+
+// also with for of loop
+const withdrawlsFor = [];
+for (const withdrw of movements) {
+  if (withdrw < 0) withdrawlsFor.push(withdrw);
+}
+console.log(withdrawlsFor);
