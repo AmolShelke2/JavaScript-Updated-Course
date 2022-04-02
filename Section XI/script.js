8d71a6cda3e6b46fdb87ea8f8d1a96cc6a58e339
Amol shelke <shelkeamol897@gmail.com>
2022-04-02 20:44:19 +0530

    Update the closeBtn implementation

--- Section XI/script.js
+++ Section XI/script.js
@@ -189,6 +189,29 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+// The findIndex() method
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // .indexOf(23)
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
 /////////////////////////////////////////////////
 // LECTURES
 
@@ -603,5 +626,3 @@ const accountMap = accounts.map(function (acc) {
   }
 });
 */
-
-// The findIndex() method
