cbe7c233cf73b78abb39e58396e66719219efe77
Amol shelke <shelkeamol897@gmail.com>
2022-04-07 19:51:56 +0530

    Update Converting and Checking Numbers

--- Section XII/script.js
+++ Section XII/script.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -253,3 +253,43 @@ btnSort.addEventListener('click', function (e) {
 // LECTURES
 
 // Converting and Checking Numbers
+
+console.log(23 === 23.0); // True
+
+// Base 10 - 0 to 9, 1/10 = 0.1 3/10 = 3.33333333
+// Binary base 2 - 0 1
+
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3);
+
+// Strings to Number
+console.log(Number('23')); // Number
+console.log(+'23'); // Same
+
+// Parsing
+console.log(Number.parseInt('30px', 10));
+console.log(Number.parseInt('e24', 10));
+
+console.log(Number.parseInt(' 2.5rem '));
+console.log(Number.parseFloat(' 2.5rem   '));
+
+// Parsing with float
+console.log(Number.parseFloat(`2.5rem`));
+console.log(Number.parseFloat('   2.3'));
+console.log(parseFloat('2.4rem'));
+
+// Check if value is NAN
+console.log(Number.isNaN(20));
+console.log(Number.isNaN('20'));
+console.log(Number.isNaN(+'20x'));
+console.log(Number.isNaN(23 / 0));
+
+// Checking if value is number
+console.log(isFinite(20));
+console.log(isFinite(+'20XS'));
+console.log(isFinite(23 / 0));
+
+// checking if value is integer
+console.log(Number.isInteger(23));
+console.log(Number.isInteger(23.0));
+console.log(Number.isInteger(23 / 0));
