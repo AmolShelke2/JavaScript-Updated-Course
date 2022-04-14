8fefc8f3f837c1b1754b1931cb5b4b750a583eed
Amol shelke <shelkeamol897@gmail.com>
2022-04-14 22:03:09 +0530

    Removed the for loop and added forEach loop

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -30,4 +30,4 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// Learning Advanced DOM
+// How DOM Actually works behind the scenes
