6b1a732b574457e1eb541e990f5630c80c8e561f
Amol shelke <shelkeamol897@gmail.com>
2022-04-20 20:56:50 +0530

    Update Event Propagation with currentTarget

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -161,7 +161,7 @@ logo.className = 'Jonas';
 */
 
 // Types of events and Event Handlers
-
+/*
 const h1 = document.querySelector('h1');
 
 const h1Alert = function (e) {
@@ -187,4 +187,29 @@ setTimeout(() => h1.removeEventListener('mouseenter', h1Alert), 3000);
 //
 // Event Propagation Bubbling and capturing
 
+*/
 // Event Propagation in practise
+
+// rgb(255, 255, 255)
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Links', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Container', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Nav', e.target, e.currentTarget);
+});
