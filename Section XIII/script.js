488ae94a08d29db757ae9b7b0adc4cf40b70d0af
Amol shelke <shelkeamol897@gmail.com>
2022-04-21 18:17:07 +0530

    Update page navigation

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTO = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -32,10 +34,7 @@ document.addEventListener('keydown', function (e) {
 
 // implementing the smoothly scroll
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
-
-btnScrollTo.addEventListener('click', function (e) {
+btnScrollTO.addEventListener('click', function (e) {
   const s1coards = section1.getBoundingClientRect();
   console.log(s1coards);
 
@@ -62,6 +61,21 @@ btnScrollTo.addEventListener('click', function (e) {
   // });
 
   section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// ////////////////////
+
+// page navigation
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+    });
+  });
 });
 
 ///
