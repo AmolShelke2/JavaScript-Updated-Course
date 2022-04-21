dc3fe209cd40ff9d622751fdd0c4d3a2421f6258
Amol shelke <shelkeamol897@gmail.com>
2022-04-21 20:26:42 +0530

    Update Going downwords and Closest method

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -248,3 +248,19 @@ document.querySelector('.nav').addEventListener('click', function (e) {
 */
 
 // Learning DOM Traversing
+
+const h1 = document.querySelector('h1');
+
+// Going Downwords
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+// Going Upwards
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
