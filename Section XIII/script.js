033bf902d0b83a5606c2a13afd83d069abee2a20
Amol shelke <shelkeamol897@gmail.com>
2022-04-22 22:32:31 +0530

    Update guard clause & Active tab

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -99,7 +99,19 @@ const tabs = document.querySelectorAll('.operations__tab');
 const tabsContainer = document.querySelector('.operations__tab-container');
 const tabsContent = document.querySelectorAll('.operations__content');
 
-tabs.forEach(t => t.addEventListener('click', () => console.log('Tab')));
+// tabs.forEach(t => t.addEventListener  ('click', () => console.log('Tab')));
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Active Tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  clicked.classList.add('operations__tab--active');
+});
 
 ///
 // How DOM Actually works behind the scenes
