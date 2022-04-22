86e2f82499ea2b21389656169f58afbc140324e2
Amol shelke <shelkeamol897@gmail.com>
2022-04-22 22:44:50 +0530

    Update the tabbed component

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -103,17 +103,19 @@ const tabsContent = document.querySelectorAll('.operations__content');
 
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
-  console.log(clicked);
 
   // Guard clause
   if (!clicked) return;
 
-  // Active Tab
+  // Remove active classes
   tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate Tab
   clicked.classList.add('operations__tab--active');
 
   // Activate Content Area
-  console.log(clicked.dataset.tab);
+
   document
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
