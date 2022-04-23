312384617a9f72662876f4932d593f6ca8546e8b
Amol shelke <shelkeamol897@gmail.com>
2022-04-23 14:14:38 +0530

    Refactored the code

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -122,32 +122,27 @@ tabsContainer.addEventListener('click', function (e) {
 });
 
 // New Fade Animation
-
-nav.addEventListener('mouseover', function (e) {
+const handleHover = function (e, opacity) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach(el => {
-      if (el !== link) el.style.opacity = 0.5;
+      if (el !== link) el.style.opacity = opacity;
     });
-    logo.style.opacity = 0.5;
+    logo.style.opacity = opacity;
   }
+};
+
+nav.addEventListener('mouseover', function (e) {
+  handleHover(e, 0.5);
 });
 
 nav.addEventListener('mouseout', function (e) {
-  if (e.target.classList.contains('nav__link')) {
-    const link = e.target;
-    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
-    const logo = link.closest('.nav').querySelector('img');
-
-    siblings.forEach(el => {
-      if (el !== link) el.style.opacity = 1;
-    });
-    logo.style.opacity = 1;
-  }
+  handleHover(e, 1);
 });
+
 ///
 // How DOM Actually works behind the scenes
 // written all notes in book
