0614e6c0c6c3bf4dcbf5e1236eaaaf70da777d3a
Amol shelke <shelkeamol897@gmail.com>
2022-04-27 17:51:22 +0530

    Added Lazy loading images

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -193,7 +193,31 @@ allsection.forEach(function (section) {
 });
 
 // Lazy Loading of images
-// Lets implement
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  // replace the src attribute with data-src
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+
+  observer.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '-200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
 
 // How DOM Actually works behind the scenes
 // written all notes in book
