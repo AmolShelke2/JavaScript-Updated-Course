ac37e17318c89422bffbe29aecf7f1c82655b4dc
Amol shelke <shelkeamol897@gmail.com>
2022-05-03 13:54:12 +0530

    Added Lifecycle of Events

--- Section XIII/script.js
+++ Section XIII/script.js
@@ -514,3 +514,16 @@ console.log(h1.parentElement.children);
 document.addEventListener('DOMContentLoaded', function (e) {
   console.log('HTML parsed and DOM tree built', e)
 })
+
+window.addEventListener('load', function (e) {
+  console.log(
+    'Page fully loade', e
+  )
+})
+
+
+// window.addEventListener('beforeunload', function (e) {
+//   e.preventDefault()
+//   console.log(e)
+//   e.returnValue = ''
+// })
