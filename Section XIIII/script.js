fad957fca3026b3d1b042ee33762d0df57c02532
Amol shelke <shelkeamol897@gmail.com>
2022-05-11 21:19:14 +0530

    Update some defination

--- Section XIIII/script.js
+++ Section XIIII/script.js
@@ -3,3 +3,11 @@
 'use strict';
 
 // Learning What is Object-oriented programming
+
+/* 
+Object-oriented programming(OOP) is a programming paradigm based on the 
+concept of objects.
+
+We use objects to model(describe) real-world abstract features
+
+*/
