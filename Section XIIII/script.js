94dc93c9e5d6d9411a775e8b88b79085df72eb4b
Amol shelke <shelkeamol897@gmail.com>
2022-05-12 19:59:01 +0530

    Added Example for explanation

--- Section XIIII/script.js
+++ Section XIIII/script.js
@@ -11,3 +11,19 @@ concept of objects.
 We use objects to model(describe) real-world abstract features
 
 */
+
+// Example
+
+const user = {
+  // data
+  user: 'Amol',
+  passWord: 'dk23s',
+
+  login(passWord) {
+    // login logic
+  },
+
+  sendMessage(str) {
+    // sending logic
+  }, // Behaviour
+};
