88a695cb63ea27628afa9958514305336829d013
Amol shelke <shelkeamol897@gmail.com>
2022-05-24 13:47:24 +0530

    Update Inheritence between classes

--- Section XIIII/script.js
+++ Section XIIII/script.js
@@ -377,3 +377,23 @@ console.log(ford);
 */
 
 // Inheritence between classes constructor function
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, course) {
+  Person.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+Student.prototype.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+const mike = new Student('Mike', 2002, 'Computer science');
+mike.introduce();
