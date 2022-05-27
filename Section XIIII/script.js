c78a3cd83d29739f98ddc9d74741b4d02288ca5f
Amol shelke <shelkeamol897@gmail.com>
2022-05-27 21:50:57 +0530

    update Another class example

--- Section XIIII/script.js
+++ Section XIIII/script.js
@@ -581,3 +581,47 @@ jay.calcAge();
 */
 
 // Another class Exmaple
+/*
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // Public Interface.
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdrawl(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan Approved.`);
+    }
+  }
+}
+
+const acc1 = new Account('Amol', 'INR', 1111);
+console.log(acc1);
+// acc1.movements.push(250);
+// acc1.movements.push(-140);
+acc1.deposit(250);
+acc1.withdrawl(148);
+console.log(acc1);
+
+console.log(acc1);
+console.log(acc1.pin);
+*/
