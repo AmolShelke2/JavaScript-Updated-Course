f5455cac10db08d699ebd93b2bccb318f7faa1ab
Amol shelke <shelkeamol897@gmail.com>
2022-05-27 16:34:45 +0530

    Update Inheritence between classes: Object.create

--- Section XIIII/script.js
+++ Section XIIII/script.js
@@ -551,3 +551,31 @@ amol.calcAge();
 */
 
 // Inheritence between classes: Object.create
+/*
+const PersonProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2000, 'Fininace');
+jay.introduce();
+jay.calcAge();
+
+*/
