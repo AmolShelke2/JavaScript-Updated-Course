669bcf9b8682d437a53ce8de2fdb7a25265cfd52
Amol shelke <shelkeamol897@gmail.com>
2022-05-29 17:54:39 +0530

    Update how to planweb project

--- Section Xv/script.js
+++ Section Xv/script.js
@@ -10,3 +10,5 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// Learned How to Plan a web project.
