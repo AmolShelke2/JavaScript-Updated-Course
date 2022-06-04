30c727f5c012f65e078f0167bb527e56e795019d
Amol shelke <shelkeamol897@gmail.com>
2022-06-04 21:11:06 +0530

    Displayed a map marker on click

--- Section Xv/script.js
+++ Section Xv/script.js
@@ -25,21 +25,25 @@ if (navigator.geolocation) {
       const coards = [latitude, longitude];
 
       const map = L.map('map').setView(coards, 13);
+      // console.log(map);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coards)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker(coards)
+          .addTo(map)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your position`);
     }
   );
 }
-
-// Displaying A map marker
