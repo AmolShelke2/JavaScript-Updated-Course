87daf48c7d0f0671c2f0dc5ce3c1843e56099da4
Amol shelke <shelkeamol897@gmail.com>
2022-06-05 20:48:53 +0530

    Learning project architecture

--- Section Xv/script.js
+++ Section Xv/script.js
@@ -80,3 +80,5 @@ inputType.addEventListener('change', function () {
   inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
   inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
 });
+
+// Project Architecture
