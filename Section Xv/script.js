16ee17d8afa648604c304388710532ce3faa7297
Amol shelke <shelkeamol897@gmail.com>
2022-06-08 20:42:18 +0530

    Interacted with Public Interface

--- Section Xv/script.js
+++ Section Xv/script.js
@@ -7,6 +7,7 @@
 class Workout {
   date = new Date();
   id = (Date.now() + '').slice(-10);
+  clicks = 0;
 
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat, lng]
@@ -22,6 +23,10 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+
+  click() {
+    this.clicks++;
   }
 }
 
@@ -288,6 +293,9 @@ class App {
         duration: 1,
       },
     });
+
+    // using the public interface
+    workout.click();
   }
 }
 
