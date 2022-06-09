881d43d64f945443019ca127207f44bf84c961fa
Amol shelke <shelkeamol897@gmail.com>
2022-06-09 22:03:01 +0530

    Created first Ajax call with XMLHttpRequest

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -4,3 +4,14 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 // Our First Ajax call
+
+/////////////////
+
+const request = new XMLHttpRequest();
+request.open('GET', 'https://restcountries.com/v3.1/name/india');
+request.send();
+
+request.addEventListener('load', function () {
+  const data = JSON.parse(request.responseText);
+  console.log(data);
+});
