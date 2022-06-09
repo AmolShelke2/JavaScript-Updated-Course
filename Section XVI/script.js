1a62dc60afc600da3de3153005e701334563022d
Amol shelke <shelkeamol897@gmail.com>
2022-06-09 23:55:39 +0530

    Fixing Error 404 not found

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -8,10 +8,26 @@ const countriesContainer = document.querySelector('.countries');
 /////////////////
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
+request.open('GET', 'https://restcountries.com/v3.1/name/Portugal');
 request.send();
 
 request.addEventListener('load', function () {
-  const data = JSON.parse(request.responseText);
+  const [data] = JSON.parse(request.responseText);
   console.log(data);
+
+  const html = ` <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span></p>
+            <p class="country__row"><span>💰</span></p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = '1';
 });
