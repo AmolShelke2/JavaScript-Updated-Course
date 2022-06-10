e39ca48a17b5c68559ea90924c6ab4d1220507ef
Amol shelke <shelkeamol897@gmail.com>
2022-06-10 12:27:46 +0530

    Rewriting all code

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -4,32 +4,3 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 // Our First Ajax call
-
-/////////////////
-
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/Portugal');
-request.send();
-
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(request.responseText);
-  console.log(data);
-
-  const html = ` <article class="country">
-          <img class="country__img" src="${data.flags.svg}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)}M people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.por}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies.EUR.name
-            }</p>
-          </div>
-        </article>`;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = '1';
-});
