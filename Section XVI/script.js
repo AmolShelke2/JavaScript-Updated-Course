373ca3f0bf1c47299691e17daf69714751d784c2
Amol shelke <shelkeamol897@gmail.com>
2022-06-11 13:30:30 +0530

    Update CallBack Hell

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -38,11 +38,12 @@ const getCountryData = function (country) {
 getCountryData('Bharat');
 */
 
+/*
 // Callback Hell
 
-const renderCountry = function (data) {
+const renderCountry = function (data, className = '') {
   const html = `
-     <article class="country">
+     <article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -50,10 +51,8 @@ const renderCountry = function (data) {
             <p class="country__row"><span>👫</span>${(
               +data.population / 1000000
             ).toFixed(1)}M people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
-            <p class="country__row"><span>💰</span>${
-              data.currencies.INR.name
-            }</p>
+            <p class="country__row"><span>🗣️</span>${data.languages}</p>
+            <p class="country__row"><span>💰</span>${data.currencies}</p>
           </div>
         </article>
   `;
@@ -63,6 +62,7 @@ const renderCountry = function (data) {
 };
 
 const getCountryAndNeighbour = function (country) {
+  // Ajax call country 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
   request.send();
@@ -70,8 +70,43 @@ const getCountryAndNeighbour = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
+
+    // Render country 1
     renderCountry(data);
+
+    // Get neighbour country
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    // Ajax call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    // Nested Callbacks
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+
+      renderCountry(data2, 'neighbour');
+    });
   });
 };
 
-getCountryAndNeighbour('Bharat');
+getCountryAndNeighbour('usa');
+
+// Example
+setTimeout(() => {
+  console.log('One second Passed');
+  setTimeout(() => {
+    console.log('Two second Passed');
+    setTimeout(() => {
+      console.log('Three second Passed');
+      setTimeout(() => {
+        console.log('Four second Passed');
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
+*/
