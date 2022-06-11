a0726b0cfe0acf7b82996f2b0abdb20a47f56266
Amol shelke <shelkeamol897@gmail.com>
2022-06-11 11:40:14 +0530

    Welcome to callBack hell

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -2,7 +2,7 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
-
+/*
 // Our First Ajax call
 const getCountryData = function (country) {
   const request = new XMLHttpRequest();
@@ -36,7 +36,42 @@ const getCountryData = function (country) {
 };
 
 getCountryData('Bharat');
-
-//Learned How the web works Request and Responses
+*/
 
 // Callback Hell
+
+const renderCountry = function (data) {
+  const html = `
+     <article class="country">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages.hin}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies.INR.name
+            }</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
+  });
+};
+
+getCountryAndNeighbour('Bharat');
