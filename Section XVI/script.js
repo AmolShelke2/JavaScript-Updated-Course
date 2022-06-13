a85e5d7fe38dcf15e2187220bd1f6206ecacce7a
Amol shelke <shelkeamol897@gmail.com>
2022-06-13 13:05:36 +0530

    Update Final Solution

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -282,19 +282,25 @@ GOOD LUCK 😀
 const whereAmI = function (lat, lan) {
   fetch(`https://geocode.xyz/${lat},${lan}?geoit=json`)
     .then(response => {
-      if (!response.ok) throw new Error(`You are not in valid country`);
+      if (!response.ok)
+        throw new Error(`Problem with geocoding ${response.status}`);
       return response.json();
     })
     .then(data => {
       console.log(data);
       console.log(`You are in ${data.country}, ${data.state}`);
-      if (!data.country) throw new Error('The country is not valid');
+
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
     })
-    .catch(err => {
-      console.log(err);
-    });
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    // .then(data => renderCountry(data[0]))
+    .catch(err => console.log(`${err.message}`));
 };
 
 whereAmI(19.037, 72.873);
 whereAmI(52.508, 13.381);
 whereAmI(-33.933, 18.474);
+whereAmI(2, 1);
