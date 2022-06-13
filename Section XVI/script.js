324bd974d923796bb88e61fe9d2a4c8b190ce205
Amol shelke <shelkeamol897@gmail.com>
2022-06-13 15:49:06 +0530

    Asynchronous Behind the scenes

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -280,7 +280,7 @@ GOOD LUCK 😀
 */
 
 // Solution
-
+/*
 const whereAmI = function (lat, lan) {
   fetch(`https://geocode.xyz/${lat},${lan}?geoit=json`)
     .then(response => {
@@ -305,5 +305,7 @@ const whereAmI = function (lat, lan) {
 whereAmI(19.037, 72.873);
 whereAmI(52.508, 13.381);
 whereAmI(-33.933, 18.474);
-
 // whereAmI(2, 1);
+*/
+
+// Asynchronous Behind the scenes
