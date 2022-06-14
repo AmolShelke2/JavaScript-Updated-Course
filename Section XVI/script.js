5e5b3cc45e8b76cd5b54854729b32a439c92ef91
Amol shelke <shelkeamol897@gmail.com>
2022-06-14 22:51:59 +0530

    Promisified the geolocation API

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -1,34 +1,34 @@
-// 'use strict';
+'use strict';
 
-// const btn = document.querySelector('.btn-country');
-// const countriesContainer = document.querySelector('.countries');
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
-// // render country
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//      <article class="country ${className}">
-//           <img class="country__img" src="${data.flags.svg}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name.common}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//               +data.population / 1000000
-//             ).toFixed(1)}M people</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages}</p>
-//             <p class="country__row"><span>💰</span>${data.currencies}</p>
-//           </div>
-//         </article>
-//   `;
+// render country
+const renderCountry = function (data, className = '') {
+  const html = `
+     <article class="country ${className}">
+          <img class="country__img" src="${data.flags.svg}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages}</p>
+            <p class="country__row"><span>💰</span>${data.currencies}</p>
+          </div>
+        </article>
+  `;
 
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   // countriesContainer.style.opacity = 1;
-// };
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  // countriesContainer.style.opacity = 1;
+};
 
-// // render error text
-// const renderError = function (msg) {
-//   countriesContainer.insertAdjacentText('beforeend', msg);
-//   // countriesContainer.style.opacity = 1;
-// };
+// render error text
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  // countriesContainer.style.opacity = 1;
+};
 
 /*
 // Our First Ajax call
@@ -381,3 +381,49 @@ Promise.reject(new Error('Problem!')).catch(x => console.error(x));
 */
 
 // Promisifying the Geolocation API
+// navigator.geolocation.getCurrentPosition(
+//   position => console.log(position),
+//   err => console.log(err)
+// );
+
+// console.log('Getting position');
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   position => resolve(position),
+    //   err => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+getPosition().then(pos => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+
+      return fetch(`https://geocode.xyz/${lat},${lng}?geoit=json`);
+    })
+    .then(response => {
+      if (!response.ok)
+        throw new Error(`Problem with geocoding ${response.status}`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v3.1/name/${data.country}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.log(`${err.message}`));
+};
+
+btn.addEventListener('click', whereAmI);
