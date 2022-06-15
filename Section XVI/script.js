410224d8c4b1e161506034cff5e102bfd12b7ff4
Amol shelke <shelkeamol897@gmail.com>
2022-06-15 22:15:48 +0530

    Update Running promises in parallel

--- Section XVI/script.js
+++ Section XVI/script.js
@@ -531,6 +531,7 @@ createImage('img/img-1.jpg')
 //   console.log(res)
 // );
 
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -586,3 +587,35 @@ console.log('1: Will get location');
   }
   console.log('3: Finsihed getting location');
 })();
+
+*/
+// Running promises in parallel
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok)
+      //
+      throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+    // console.log(data1.capital, data2.capital, data3.capital);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('Bharat', 'portugal', 'russia');
