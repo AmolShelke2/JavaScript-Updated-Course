786fddffa3779a1b97d112245944250cce5bbea1
Amol shelke <shelkeamol897@gmail.com>
2022-06-17 21:05:08 +0530

    Update basic of npm

--- Section XVII/script.js
+++ Section XVII/script.js
@@ -82,3 +82,28 @@ const { addToCart } = require('./clean.js')
 */
 
 // Introduction to NPM
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    {
+      product: 'bread',
+      quantity: 5,
+    },
+    {
+      product: 'pizza',
+      quantity: 5,
+    },
+  ],
+  user: {
+    loggedIn: true,
+  },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
