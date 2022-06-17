0ab58ece2317f65d7f9e9f28edf41c60ad0138d9
Amol shelke <shelkeamol897@gmail.com>
2022-06-17 17:09:29 +0530

    update commonJs modules

--- Section XVII/script.js
+++ Section XVII/script.js
@@ -65,3 +65,18 @@ console.log(ShoppingCart2.shippingCost);
 */
 
 // CommonJS modules
+
+// Export
+/*
+ export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+};
+  
+// Import
+
+const { addToCart } = require('./clean.js')
+
+*/
