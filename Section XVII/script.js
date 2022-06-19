d3bf88b1c649f6972f23ef26f1a20bb63c39ff4b
Amol shelke <shelkeamol897@gmail.com>
2022-06-19 17:05:51 +0530

    reLearning the Intro to NODE

--- Section XVII/script.js
+++ Section XVII/script.js
@@ -1,7 +1,7 @@
 // Importing and exporting Modules.
 
-import './clean.js';
-import { addToCart, calcAge, totalPrice as price, QT } from './clean.js';
+import './shopingCart.js';
+import { addToCart, calcAge, totalPrice as price, QT } from './shopingCart.js';
 
 console.log('Importing Module');
 addToCart('bread', 5);
@@ -12,16 +12,16 @@ console.log(price, QT);
 
 console.log('Importing Module');
 
-import * as ShoppingCart from './clean.js';
+import * as ShoppingCart from './shopingCart.js';
 ShoppingCart.addToCart('bread', 5);
 console.log(ShoppingCart.totalPrice);
 
 // default exports
 
-import add, { addToCart, calcAge, totalPrice as price, QT } from './clean.js';
-console.log(price);
+// import add, { calcAge, totalPrice as price, QT } from './shopingCart.js';
+// console.log(price);
 
-import add, { cart, printGreeting } from './clean.js';
+import add, { cart, printGreeting } from './shopingCart.js';
 add('pizza', 2);
 add('bread', 2);
 add('apples', 2);
@@ -30,7 +30,7 @@ console.log(cart);
 printGreeting('Amol', 17, 'Software Engineer');
 
 // Module pattern
-
+/*
 const ShoppingCart2 = (function () {
   const cart = [];
   const shippingCost = 10;
@@ -56,11 +56,13 @@ const ShoppingCart2 = (function () {
   };
 })();
 
+
 ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+*/
 
 // CommonJS modules
 
@@ -80,49 +82,21 @@ const { addToCart } = require('./clean.js')
 
 // Introduction to NPM
 
-// import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-// import { cloneDeep } from 'lodash-es';
-import { cloneDeep } from 'lodash-es';
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
 
 const state = {
   cart: [
-    {
-      product: 'bread',
-      quantity: 5,
-    },
-    {
-      product: 'pizza',
-      quantity: 5,
-    },
+    { product: 'bread', quantity: 4 },
+    { product: 'pizza', quantity: 2 },
   ],
-  user: {
-    loggedIn: true,
-  },
+  user: { loggedIn: true },
 };
 
 const stateClone = Object.assign({}, state);
 const stateDeepClone = cloneDeep(state);
-state.user.loggedIn = false;
 console.log(stateClone);
+
+stateClone.user.loggedIn = false;
+// console.log(stateClone);
+
 console.log(stateDeepClone);
-
-// console.log('HEllo');
-
-// Bundling with parcel and NPM scripts
-
-if (module.hot) {
-  module.hot.accept();
-}
-
-// Configuring Babel and Pollyfilling
-class Person {
-  #greeting = 'hey';
-  constructor(name) {
-    this.name = name;
-    console.log(`${this.#greeting}, ${this.name}`);
-  }
-}
-
-const amol = new Person('Amol');
-
-console.log('Amol' ?? null);
