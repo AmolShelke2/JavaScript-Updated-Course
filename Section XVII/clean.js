aa1cfd29193715be128cf477504b72753869af26
Amol shelke <shelkeamol897@gmail.com>
2022-06-20 16:30:56 +0530

    Updated Let's fix some bad code part 1

--- Section XVII/clean.js
+++ Section XVII/clean.js
@@ -9,56 +9,45 @@ var budget = [
   { value: -1800, description: 'New Laptop 💻', user: 'jonas' },
 ];
 
-var limits = {
+const spendingLimit = {
   jonas: 1500,
   matilda: 100,
 };
 
-var add = function (value, description, user) {
-  if (!user) user = 'jonas';
+const getLimit = user => spendingLimit?.[user] ?? 0;
+
+const addExpense = function (value, description, user = 'jonas') {
   user = user.toLowerCase();
 
-  var lim;
-  if (limits[user]) {
-    lim = limits[user];
-  } else {
-    lim = 0;
-  }
+  //   const limit = spendingLimit[user] ? spendingLimit[user] : 0;
 
-  if (value <= lim) {
-    budget.push({ value: -value, description: description, user: user });
+  if (value <= getLimit(user)) {
+    budget.push({ value: -value, description, user });
   }
 };
-add(10, 'Pizza 🍕');
-add(100, 'Going to movies 🍿', 'Matilda');
-add(200, 'Stuff', 'Jay');
+
+addExpense(10, 'Pizza 🍕');
+addExpense(100, 'Going to movies 🍿', 'Matilda');
+addExpense(200, 'Stuff', 'Jay');
 console.log(budget);
 
-var check = function () {
-  for (var el of budget) {
-    var lim;
-    if (limits[el.user]) {
-      lim = limits[el.user];
-    } else {
-      lim = 0;
-    }
-
-    if (el.value < -lim) {
-      el.flag = 'limit';
-    }
-  }
+const checkExpenses = function () {
+  for (const entry of budget)
+    if (entry.value < -getLimit(entry.user)) entry.flag = 'limit';
 };
-check();
+
+checkExpenses();
 
 console.log(budget);
 
-var bigExpenses = function (limit) {
-  var output = '';
-  for (var el of budget) {
-    if (el.value <= -limit) {
-      output += el.description.slice(-2) + ' / '; // Emojis are 2 chars
-    }
-  }
+const LogbigExpenses = function (bigLimit) {
+  let output = '';
+  for (const el of budget)
+    output += el.value <= -bigLimit ? el.description.slice(-2) + ' / ' : '';
+
   output = output.slice(0, -2); // Remove last '/ '
   console.log(output);
 };
+
+LogbigExpenses(10000);
+LogbigExpenses(100);
