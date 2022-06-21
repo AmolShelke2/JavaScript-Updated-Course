fd0b6320b496a57c2a78546ba31cb10dd770ad8f
Amol shelke <shelkeamol897@gmail.com>
2022-06-21 23:26:22 +0530

    Added starter files of Forkify

---
+++ Section XVIII Forkify/src/js/controller.js
@@ -0,0 +1,9 @@
+const recipeContainer = document.querySelector(".recipe");
+
+const timeOut = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took to long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
