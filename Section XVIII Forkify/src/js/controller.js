edaf8d2aa4bd93a9f3507afb8b62b996ac0e2ae7
Amol shelke <shelkeamol897@gmail.com>
2022-06-22 16:20:06 +0530

    Updated Loading a Recipe from API

--- Section XVIII Forkify/src/js/controller.js
+++ Section XVIII Forkify/src/js/controller.js
@@ -7,3 +7,34 @@ const timeOut = function (s) {
     }, s * 1000);
   });
 };
+
+/////////////////////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bce7f'
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} ${res.status}`);
+
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
