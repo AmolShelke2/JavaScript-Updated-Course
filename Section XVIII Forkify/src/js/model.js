c3fb077ed044b04c390c3007bdfc51cba57e53de
Amol shelke <shelkeamol897@gmail.com>
2022-07-06 20:23:46 +0530

    Fixed the pagination view bug

--- Section XVIII Forkify/src/js/model.js
+++ Section XVIII Forkify/src/js/model.js
@@ -50,6 +50,8 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+
+    state.search.page = 1;
   } catch (err) {
     console.error(`${err} ❤️‍🔥❤️‍🔥❤️‍🔥`);
     throw err;
