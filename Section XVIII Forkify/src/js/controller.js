f7d3503cd25768522b481fbe4c864303308e7069
Amol shelke <shelkeamol897@gmail.com>
2022-07-06 19:10:57 +0530

    Feat: Implemented DOM Algorithm

--- Section XVIII Forkify/src/js/controller.js
+++ Section XVIII Forkify/src/js/controller.js
@@ -68,7 +68,8 @@ const controlServings = function (newServings) {
   model.updateServings(newServings);
 
   // update the recipe view.
-  recipeView.render(model.state.recipe);
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
--- Section XVIII Forkify/src/js/views/view.js
+++ Section XVIII Forkify/src/js/views/view.js
@@ -8,8 +8,42 @@ export default class View {
 
     this._data = data;
     const markup = this._generateMarkup();
+
     this._clear();
     this._parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  update(data) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+
+    const newDom = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDom.querySelectorAll('*'));
+    const currenteElements = Array.from(
+      this._parentElement.querySelectorAll('*')
+    );
+
+    newElements.forEach((newEl, i) => {
+      const curEl = currenteElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        curEl.textContent = newEl.textContent;
+      }
+
+      if (!newEl.isEqualNode(curEl)) {
+        console.log(Array.from(newEl.attributes));
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
   }
 
   _clear() {
