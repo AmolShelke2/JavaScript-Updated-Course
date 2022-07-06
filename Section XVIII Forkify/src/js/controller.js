d1dc2740e80c63787d3b0d0f27427e07f82178af
Amol shelke <shelkeamol897@gmail.com>
2022-07-06 19:49:01 +0530

    Updated final algorithm

--- Section XVIII Forkify/src/js/controller.js
+++ Section XVIII Forkify/src/js/controller.js
@@ -19,6 +19,9 @@ const controlRecipes = async function () {
     if (!id) return;
     recipeView.renderSpinner();
 
+    // 0) update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage());
+
     //1) Loading recipe
     await model.loadRecipe(id);
 
